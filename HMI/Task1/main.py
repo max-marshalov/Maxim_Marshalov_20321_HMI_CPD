8c1a19e808cdebd8a05dbadc55f3280ca07491b5
Maxim <marshalmaxim@gmail.com>
2023-10-01 15:34:02 +0300

    Add headres

--- HMI/Task1/main.py
+++ HMI/Task1/main.py
@@ -1,3 +1,9 @@
+##
+# @author Maxim Marshalov <marshalmaxim@gmail.com>
+ # @file Description
+ # @desc Created on 2023-10-01 3:33:49 pm
+ # @copyright SMTU
+ #
 from views.App import App
 import customtkinter as ctk
 if __name__ == "__main__":
