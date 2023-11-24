ab90de472331c0164ea1ec1e4e1c37e0abbc0c1a
Maxim <marshalmaxim@gmail.com>
2023-11-24 12:48:58 +0300

    fix params

--- CPD/Task4/server.py
+++ CPD/Task4/server.py
@@ -35,12 +35,12 @@ class VideoServer():
 
     def readData(self):
         data = bytes()
-        payload_size = struct.calcsize("H") 
+        payload_size = struct.calcsize("!L") 
         while len(data) < payload_size:
             data += self.clientConnection.readAll().data()
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
-        msg_size = struct.unpack("H", packed_msg_size)[0]
+        msg_size = struct.unpack("!L", packed_msg_size)[0]
         while len(data) < msg_size:
             data += self.clientConnection.readAll().data()
         frame_data = data[:msg_size]
