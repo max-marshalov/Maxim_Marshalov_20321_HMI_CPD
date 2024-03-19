1e319b11a2d0018accfc62ecca7c752196c24031
Max <marshalmaxim@gmail.com>
2024-03-19 09:48:33 +0300

    add MergeArray solve

--- CPD/Merge_Sorted_Array/src/main.rs
+++ CPD/Merge_Sorted_Array/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut nums1 = vec![1,2,3,0,0,0];
+    let nums2 = vec![2,5,6];
+    let m:i32 = 3;
+    let n:i32 = 3;
+    for i in 0..n{
+        nums1[(m + i) as usize] = nums2[i as usize];
+
+    }
+    nums1.sort();
+    
 }
